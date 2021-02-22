96d0181e35d4a2b963a9931a4254f70896888232
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-02-22 13:37:28 +0000

    make better looking choose list-type buttons

--- list_app/lib/main.dart
+++ list_app/lib/main.dart
@@ -3,6 +3,80 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ,
+    home: Lists(),
   ));
+}
+
+class Lists extends StatefulWidget {
+  @override
+  _ListsState createState() => _ListsState();
+}
+
+class _ListsState extends State<Lists> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.blue,
+                height: 100.0,
+                width: 350.0,
+                child: FlatButton(
+                  child: Text(
+                    'Unordered List',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.blue,
+                height: 100.0,
+                width: 350.0,
+                child: FlatButton(
+                  child: Text(
+                    'Numbered List',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.blue,
+                height: 100.0,
+                width: 350.0,
+                child: FlatButton(
+                  child: Text(
+                    'Checklist',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
